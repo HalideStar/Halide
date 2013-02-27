28f939ffa708dc6739f4ab451dfbe34bdefa75a1
Len Hamey <len.hamey@mq.edu.au>
2013-02-27 12:02:15 +1100

    Backward interval inference with special case for constant.

--- cpp/src/DomainInference.cpp
+++ cpp/src/DomainInference.cpp
@@ -66,36 +66,89 @@ public:
     Expr xmin;
     Expr xmax;
     std::string varname;
-    bool poison;
+    Expr poison;
     
     BackwardIntervalInference(Expr axmin, Expr axmax) : 
-        xmin(axmin), xmax(axmax), varname(""), poison(false) {}
+        xmin(axmin), xmax(axmax), varname(""), poison(const_false()) {}
 
 private:
+
     using IRVisitor::visit;
     
-    void visit(const Variable *var)
-    {
+    void visit(const Variable *op) {
         // Variable node defines the varname string - the variable for which we are
         // building the inverse function.
-        varname = var->name;
+        if (varname != "") {
+            log(0,"LH") << "Set poison to true in Variable\n";
+            poison = const_true(); // Have already seen a variable in another branch
+        }
+        varname = op->name;
     }
     
-    void visit(const Add *op)
-    {
-        if (is_const(op->b))
-        {
-            // Looking for constant on RHS of Add node.
+    void visit(const Add *op) {
+        if (is_const(op->b)) {
+            // Looking at constant on RHS of Add node.
+            // e = x + k
+            // x = e - k
             xmin = xmin - op->b;
             xmax = xmax - op->b;
             // Process the tree recursively
             op->a.accept(this);
         }
-        else
-        {
+        else {
             assert(! is_const(op->a) && "Simplify did not put constant on RHS of Add");
-            poison = true; // Expression cannot be solved as it has branches not simplified out.
+            log(0,"LH") << "Set poison to true in Add\n";
+            poison = const_true(); // Expression cannot be solved as it has branches not simplified out.
         }
+    }
+    
+    void visit(const Sub *op) {
+        // Simplify should convert x - 5 to x + -5.
+        assert(! is_const(op->b) && "Simplify left Sub of a constant");
+        if (is_const(op->a)) {
+            // e = k - x
+            // x = k - e
+            Expr new_xmin = op->a - xmax;
+            xmax = op->a - xmin;
+            xmin = new_xmin;
+            op->b.accept(this);
+        }
+        else
+            poison = const_true();
+    }
+    
+    void visit(const Mul *op) {
+        assert(! is_const(op->a) && "Simplify did not put constant on RHS of Mul");
+        if (is_const(op->b)) {
+            // e = x * k
+            // x = e / k
+            // As a range, however, it is ceil(min/k) , floor(max/k)
+            // We assume positive remainder semantics for division,
+            // which is a valid assumption if the interval bounds are positive
+            // Under these semantics, integer division always yields the floor.
+            xmin = (xmin + op->b - 1) / op->b;
+            xmax = xmax / op->b;
+            op->a.accept(this);
+        }
+        else
+            poison = const_true();
+    }
+    
+    void visit(const Div *op) {
+        if (is_const(op->b)) {
+            // e = x / k
+            // x = e * k
+            // As a range, however, it is min * k to (max + 1) * k - 1
+            // This is based on assumption of positive remainder semantics for
+            // division and is intended to ensure that dividing by 2 produces a new
+            // image that has every pixel repeated; i.e. the dimension is doubled.
+            xmin = xmin * op->b;
+            xmax = (xmax + 1) * op->b - 1;
+            op->a.accept(this);
+        }
+        else
+            // e = k / x is not handled because it is not a linear transformation
+            poison = const_true();
     }
 };
 
@@ -104,67 +157,133 @@ Difference between Var and Variable.  Variable is a parse tree node.
 Var is just a name.
 */
 
-Interval backwards_interval(Expr e, Expr xmin, Expr xmax, std::string &v)
-{
+Interval backwards_interval(Expr e, Expr xmin, Expr xmax, std::string &v, Expr &poison) {
     BackwardIntervalInference infers(xmin, xmax);
     Expr e1 = simplify(e);
     
-    e1.accept(&infers);
+    // Special cases.
+    const IntImm *intimm = e1.as<IntImm>();
+    if (intimm) {
+        // A tree that is entirely a constant results in either infinite or empty interval.
+        // Constant expression means that it is either always out of bounds in the
+        // callee or always in bounds.
+        // This is only valid if the constant expression is the top of the tree;
+        // encountering a constant leaf node lower in the tree is not invertible.
+        // Set the range to be undefined and set to poison if it would be out of
+        // bounds on the callee.
+        infers.poison = intimm->value < xmin || intimm->value > xmax;
+        infers.xmin = Expr();
+        infers.xmax = Expr(); 
+    }
+    else
+        // General case: inversion of the expression.
+        e1.accept(&infers);
     
     Interval result(infers.xmin, infers.xmax);
     if (result.min.defined()) result.min = simplify(result.min);
     if (result.max.defined()) result.max = simplify(result.max);
     v = infers.varname;
+    poison = infers.poison;
+    if (poison.defined()) {
+        //log(0,"LH") << "Poison = " << poison;
+        poison = poison || make_bool(infers.defaulted);
+        //log(0,"LH") << "    simplify " << poison << '\n';
+        poison = simplify(poison);
+    }
+    else
+        poison = infers.defaulted;
 
     return result;
 }
 
 void check_interval(Expr e, Expr xmin, Expr xmax, 
-                    Expr correct_min, Expr correct_max, std::string correct_varname)
-{
+                    bool correct_poison_bool, Expr correct_min, Expr correct_max, 
+                    std::string correct_varname) {
     std::string v;
-    Interval result = backwards_interval(e, xmin, xmax, v);
+    Expr poison, correct_poison;
+    correct_poison = make_bool(correct_poison_bool);
+    Interval result = backwards_interval(e, xmin, xmax, v, poison);
     
     Expr e1 = simplify(e); // Duplicate simplification for debugging only
     log(0,"LH") << "e: " << e << "    ";
-    log(0,"LH") << "e1: " << e1 << '\n';
-    //log(0,"LH") << "min: " << result.min << "    ";
-    //log(0,"LH") << "max: " << result.max << '\n';
+    log(0,"LH") << "e1: " << e1 << "    ";
     
-    log(0,"LH") << "min: " << result.min << "    ";
-    log(0,"LH") << "max: " << result.max << '\n';
+    if (equal(poison, const_true()))
+        log(0,"LH") << "poison\n";
+    else {
+        log(0,"LH") << "poison: " << poison << "    ";
+        log(0,"LH") << "min: " << result.min << "    ";
+        log(0,"LH") << "max: " << result.max << "    ";
+        log(0,"LH") << "v: " << v << '\n';
+    }
     
     bool success = true;
-    if (!equal(result.min, correct_min)) {
-        std::cout << "Incorrect min: " << result.min << std::endl
-                  << "Should have been: " << correct_min << std::endl;
+    if (! equal(poison, correct_poison)) {
+        std::cout << "Incorrect poison: " << poison << "    "
+                  << "Should have been: " << correct_poison << std::endl;
         success = false;
     }
-    if (!equal(result.max, correct_max)) {
-        std::cout << "Incorrect max: " << result.max << std::endl
-                  << "Should have been: " << correct_max << std::endl;
-        success = false;
+    if (! correct_poison_bool) {
+        // Only bother to check the details if it is not supposed to be poison
+        if (!equal(result.min, correct_min)) {
+            std::cout << "Incorrect min: " << result.min << "    "
+                      << "Should have been: " << correct_min << std::endl;
+            success = false;
+        }
+        if (!equal(result.max, correct_max)) {
+            std::cout << "Incorrect max: " << result.max << "    "
+                      << "Should have been: " << correct_max << std::endl;
+            success = false;
+        }
+        if (v != correct_varname) {
+            std::cout << "Incorrect variable name: " << v << "    "
+                      << "Should have been: " << correct_varname << std::endl;
+            success = false;
+        }
     }
     assert(success && "Domain inference test failed");
 }
 
-void domain_inference_test()
-{
-    Var x("x");
+void domain_inference_test() {
+    Var x("x"), y("y");
     std::string v = "<dummy>";
     Expr cmin, cmax;
     
     // Tests of backward interval inference
-    check_interval(x, 0, 100, 0, 100, "x");
-    check_interval(x + 1, 0, 100, -1, 99, "x");
-    check_interval(1 + x, 0, 100, -1, 99, "x");
+    check_interval(5, 0, 100, false, Expr(), Expr(), "");
+    check_interval(105, 0, 100, true, Expr(), Expr(), "");
+    check_interval(x, 0, 100, false, 0, 100, "x");
+    check_interval(x + 1, 0, 100, false, -1, 99, "x");
+    check_interval(1 + x, 0, 100, false, -1, 99, "x");
+    check_interval(x - 1, 0, 100, false, 1, 101, "x");
+    check_interval(1 - x, 0, 100, false, -99, 1, "x");
     //check_interval(x + x, 0, 100, cmin, cmax, v);
-    check_interval(2 * x, 0, 100, 0, 50, "x");
-    check_interval(x * 2, 0, 100, 0, 50, "x");
-    check_interval(x / 2, 0, 100, 0, 201, "x");
-    //check_interval((x + 1) / 2, 0, 100, cmin, cmax, v);
-    //check_interval((x + 2) / 2, 0, 100, cmin, cmax, v);
-    //check_interval((2 * x + 4) / 2, 0, 100, cmin, cmax, v);
+    // Tests that use * and / should ensure that results are positive
+    // so that positive remainder semantics hold for division
+    // (until these semantics are actually implemented in Halide)
+    check_interval(2 * x, 10, 100, false, 5, 50, "x");
+    check_interval(x * 2, 10, 100, false, 5, 50, "x");
+    check_interval(x / 2, 10, 100, false, 20, 201, "x");
+    // x = 19  e = (19 + 1) / 2 = 10
+    // x = 200  e = (201) / 2 = 100
+    check_interval((x + 1) / 2, 10, 100, false, 19, 200, "x");
+    check_interval((x + 2) / 2, 10, 100, false, 18, 199, "x");
+    // (2 * x + 4) / 2 is the same as x + 2
+    check_interval((2 * x + 4) / 2, 10, 100, false, 8, 98, "x");
+    // x = 8  e = (16 + 5) / 2 = 10
+    // x = 98  e = (196 + 5) / 2 = 201 / 2 = 100
+    // This expression also simplifies to x + 2
+    check_interval((2 * x + 5) / 2, 10, 100, false, 8, 98, "x");
+    // x = 5  e = (15 + 5) / 2 = 10
+    // x = 65  e = (195 + 5) / 2 = 100   but x = 66 is too big
+    check_interval((3 * x + 5) / 2, 10, 100, false, 5, 65, "x");
+    
+    // Expression is poison because it contains a node type that 
+    // is not explicitly handled
+    check_interval(sin(x), 10, 100, true, 0, 0, "");
+    // Expression is poison because it contains more than one variable
+    // Actually, it is detected as poison by Add node because it is not x + k
+    check_interval(x + y, 0, 100, true, 0, 0, "");
     std::cout << "Domain inference test passed" << std::endl;
     return;
 }
