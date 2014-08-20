c85f01dff2421ac119e478ff7d568e1de77b3884
Len Hamey <len.hamey@mq.edu.au>
2014-08-20 15:23:12 +1000

    Add HalideFeatres.h

---
+++ src/HalideFeatures.h
@@ -0,0 +1,13 @@
+#ifndef HALIDE_FEATURES_H
+#define HALIDE_FEATURES_H
+
+# define HALIDE_DOMAIN_INFERENCE 1
+// Introduce the Clamp node used for border handing
+# define HALIDE_CLAMP_NODE 1
+
+// Adjust reference counts for circular references
+// Disable this option to overcome a bug that appears in the test
+// circular_reference_leak.cpp.
+//# define HALIDE_CIRCULAR_REFERENCE_ADJUST
+
+#endif
